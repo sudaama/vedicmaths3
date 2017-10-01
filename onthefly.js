4635ffd96efc182e1861ddbdb51022bab81e5d7f
sudaama <sudaama@hotmail.com>
2017-10-01 16:01:09 +0100

    adding on-th-fly form elements

--- onthefly.js
+++ onthefly.js
@@ -2,6 +2,7 @@
 */
 
 // Routine for generating and removing fresh numbers on the fly 
+// we need to add form elements here
 
 var addFreshNum = function(freshId, freshNumArr) {
  		return function() {
@@ -9,12 +10,19 @@ var addFreshNum = function(freshId, freshNumArr) {
       newDiv.className = "freshNum";
       // var html = '<p id="' + freshId + '" ' + 'class="freshNum" >A freshNum goes here: ' + freshNumArr + '</p>';
       var html = '<p class="freshNum" >A freshNum goes here: ' + freshNumArr + '</p>';
+      html += '<form id="multiplicandFrm" action="#">' + '<div id="multiplicand"></div>' + '<div id="numLabel"></div>' +
+    '</form>' + '<div id="dw1" class="margTop"></div>';
       newDiv.innerHTML = html;
       var butElem = document.getElementById(freshId);
       butElem.appendChild(newDiv)
     };
  };
  
+
+
+
+
+
 var removeFreshNum = function(divname) {                          // create a closure...remove a digit.
     return function() {
         var getparentDiv = document.getElementById(divname);
