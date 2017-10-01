9578805498632003c3eecff06ae321b3a3808ca0
sudaama <sudaama@hotmail.com>
2017-10-01 21:37:02 +0100

    more...

--- onthefly.js
+++ onthefly.js
@@ -1,30 +1,31 @@
-/* This forms the basis of a function that can that multiple fresh numbers 
-*/
+/* This forms the basis of a function that can that multiple fresh numbers */
 
 // Routine for generating and removing fresh numbers on the fly 
 // we need to add form elements here
+// need to replace fresshNumArr with a number object that has methods to change it, as before
+// ie. ord, mix, vin
 
 window.onload = addFreshNum, removeFreshNum
 
-var addFreshNum = function(freshFrmId, freshNumId, freshNumArr) {
+var addFreshNum = function(freshNumArr, multNumId) {
  		return function() {
+ 		var freshFrmId = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for form id
+ 		var freshNumId = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for form id
+
     	var newDiv = document.createElement('div');
-      newDiv.className = "freshNum";
-      // var html = '<p id="' + freshId + '" ' + 'class="freshNum" >A freshNum goes here: ' + freshNumArr + '</p>';
-      var html = '<p class="freshNum" >A freshNum goes here: ' + freshNumArr + '</p>';
-    html += '<form id=' + '"freshFrmId_' + freshFrmId[0] + '"' + 'action="#">' + '<div id="freshNumId_' + freshNumId[0] +'"> </div>' + '<div id="numLabel"></div>' +
-    '</form>' + '<div id="dw1" class="margTop"></div>';
+      	newDiv.className = "freshNum";
+      	var html = '<p class="freshNum" >A freshNum goes here: ' + freshNumArr + '</p>';
+    	html += '<form id=' + '"freshFrmId_' + freshFrmId + '"' + 'action="#">' 
+    	+ '<div id="freshNumId_' + freshNumId +'"> </div>' + '<div id="numLabel"></div>' +
+    	'</form>' + '<div id="dw1" class="margTop"></div>';
       
-      newDiv.innerHTML = html;
-      var butElem = document.getElementById("freshFrmId_1");
-      butElem.appendChild(newDiv)
-    };
+      	newDiv.innerHTML = html;
+
+      	var butElem = document.getElementById(multNumId);	// insert the id of the number container
+      	butElem.appendChild(newDiv)
+    	};
  };
  
-
-
-
-
 
 var removeFreshNum = function(divname) {                          // create a closure...remove a digit.
     return function() {
--- ontheflyImplement.js
+++ ontheflyImplement.js
@@ -1,15 +1,17 @@
 
-var freshFrmId = [1, 2, 3, 4, 5]; // create Id of numbers
-var freshNumArray = [1, 2, 3, 4];		// this represents a typical array of signed digits 
-var freshNumId = [1, 2, 3, 4];
+// we have now randomly created form and number ids within onthefly.js
 
-var addFreshNumber = addFreshNum(freshFrmId, freshNumId, freshNumArray);
+
+var freshNumArray = [1, 2, 3, 4];		// this represents a typical array of signed digits 
+var multNumId = "multNumId";			// id of the multi number container 		
+
+var addFreshNumber = addFreshNum(freshNumArray, multNumId);
 
 var getButtId = document.getElementById("buttFresh");
 getButtId.addEventListener("click", addFreshNumber);
 
 
-var removeFreshNumber = removeFreshNum(freshNumId);	// use the id of the container div and remove last child
+var removeFreshNumber = removeFreshNum(multNumId);	// use the id of the container div and remove last child
 
 var getButtIdRemove = document.getElementById("buttRemoveFresh");
 getButtIdRemove.addEventListener("click", removeFreshNumber); 		// use the function variable 
