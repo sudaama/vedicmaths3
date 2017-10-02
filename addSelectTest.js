095d92622a20f3554957542e310b344fffcc51e5
sudaama <sudaama@hotmail.com>
2017-10-02 15:04:40 +0100

    incorp add and remove more

---
+++ addSelectTest.js
@@ -0,0 +1,62 @@
+/* numGen generates an array of digits, 0 through 9 inclusive
+*  addSelectFunk function takes a single parameter, id, the location of where the signed digits will be formed
+*  the function uses each of the digits generated using numGen and individually wraps them in option tags, 
+*  all enclosed in select tags.
+*  removeSelectFunk removes the last child in the parent element.   
+*
+*/
+window.onload = addSelectFunk, removeSelectFunk;
+
+var numGen = function ()  {
+    var num, numArray = new Array(), index;
+       
+    for(index = 0; index < 10; index++) {
+        numArray[index] = index;
+    }
+    
+    return numArray;
+};
+
+//var buttonId = "butt";                          // Identity of Add Digit Button 
+//var readOut = "multiplicand";                   // Identity of first child first form multiplicanFrm
+// var buttR = "buttRemove";                       // Identity of Remove Digit Button
+
+
+// Essentially, this function displays the individual digit....using a closure, returning a function
+// digitReadout is where display will be output
+var addSelectFunk = function(digitReadOut) {
+    return function() {         // Create a closure so we can use in addEventListener...function (2)
+        var newDiv = document.createElement('div');
+        var butElem = document.getElementById(digitReadOut);         // readOut is id of Input section
+        // var butElem = document.getElementById("testId");         // readOut is id of Input section
+        newDiv.className = "next2each rounded";
+        var html = '<select class="num rounded" name="digit[]">'; 
+        var num = numGen();
+        for(var i = 0; i < num.length; i++) {
+            html += "<option value='" + num[i] + "'" + "id=" + '"' + num[i] + '"' + ">" + num[i] + "</option>";
+        }
+        html += '</select>';
+
+        // An array called sign will be created containing the individual digit signs  
+        var html2 = '<select  class="sign rounded" name="sign[]">';
+        html2 += "<option value='" + '' + "'>" + '+' + "</option>" + "<option value='" + '-' + "'>" + '-' + "</option>";
+        html2 += '</select>';
+            
+        // Concatenate the html statements
+        html = html2 + html;
+            
+        // Place the html into the newly created div called newDiv 
+        newDiv.innerHTML= html;
+
+        
+        butElem.appendChild(newDiv);                                // test1
+    };                                                              // end of function (2)
+};                                                                  // end od addSelectFunk
+
+
+var removeSelectFunk = function(divname) {                          // create a closure...remove a digit.
+    return function() {
+        var getparentDiv = document.getElementById(divname);
+        getparentDiv.removeChild( getparentDiv.lastChild );
+    };
+};
--- onthefly.js
+++ onthefly.js
@@ -11,32 +11,61 @@ var addFreshNum = function(freshNumArr, multNumId) {
  		return function() {
  		var freshFrmId = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for form id
  		var freshNumId = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for Num id
+ 		var buttIdAdd = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for buttAdd id
+ 		var buttIdRemove = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for buttRemove id
+ 		var mixedId = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for mixedId id
+ 		var ordinaryId = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for ordinaryId id
+ 		var vinculumId = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for vinculumId id
+ 		var digitDisplayId = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for digitDisplay id
 
 
     	var newDiv = document.createElement('div');
       	newDiv.className = "freshNum";
-      	var html = '<p class="freshNum" >A freshNum goes here: ' + freshNumArr + '</p>';
+      	// var html = '<p class="freshNum" >A freshNum goes here: ' + freshNumArr + '</p>';
+    	var html = '';
     	html += '<form id=' + '"freshFrmId_' + freshFrmId + '"' + 'action="#">' 
     	+ '<div id="freshNumId_' + freshNumId +'"> </div>' + '<div id="numLabel"></div>' +
-    	'</form>' + '<div id="dw1" class="margTop"></div>';
-    	// temporary tester code for buttIdAdd and buttIdRemove
-    	var buttIdAdd = 1;
-    	var buttIdRemove = 1;
-    	
-    	html += '<h4>Vedic Number Transmutor</h4>' + '<hr>' + 
-                   '<div><p class="text-vedic"><button type="button" id="butt_' + buttIdAdd + '"' + 
+    	'</form>';
+
+    	html +=	   '<div><p class="text-vedic"><button type="button" id="butt_' + buttIdAdd + '"' + 
                    'class="rounded padMe addColor" data-toggle="tooltip" data-placement="left"' + 
                    'title="Add a digit: positive (+) or negative (-)">Add Digit (+)</button>';
 
-    	html += '<button type="button" id="buttRemove_' + buttIdRemove + '"' +  
-    		'class="rounded padMe remColor" data-toggle="tooltip" data-placement="right"' + 
-    		'title="Remove a digit">Remove Digit (-)</button></p></div>';
+    	html += 	'<button type="button" id="buttRemove_' + buttIdRemove + '"' +  
+    				'class="rounded padMe remColor" data-toggle="tooltip" data-placement="right"' + 
+    				'title="Remove a digit">Remove Digit (-)</button></p>' +
+    				'</div>';
+
+    	html += 	'<div><p class="text-vedic"><button type="button" id="displayMixed_' + mixedId + '"' + 'class="rounded padMe"' + 
+    				'data-toggle="tooltip" data-placement="left" title="Display the mixed number i.e. ' + 
+    				'with (+) and (-) digits" data-placement="left" >Display: <span class="text-vedic">' + 
+    				'Mixed Number</span></button>';
+
+    	html += 	'<button type="button" id="displayOrdinary_' + ordinaryId + '"' + 'class="rounded padMe"' + 
+    				'data-toggle="tooltip" data-placement="bottom" title="Display the ordinary number i.e. ' +
+    				'with digits either all positive(+) or either all negative(-)" data-placement="left">' +
+    				'Display: <span class="text-vedic">Ordinary Number</span></button>';
+
+    	html += 	'<button type="button" id="displayVinculum_' + vinculumId + '"' + 'class="rounded padMe"' + 
+    				'data-toggle="tooltip" data-placement="right" title="Display the Vinculum number i.e.  ' + 
+    				'with (+) and (-) digits and each digit <= 5">Display: <span class="text-vedic">' + 
+    				'Vinculum Number</span></button></p></div>';
+
+    	html += 	'<div id="digitDisplayId_' + digitDisplayId + '"' + '>';
+    	html += '<hr>';
       
       	newDiv.innerHTML = html;
 
       	var butElem = document.getElementById(multNumId);	// insert the id of the number container
-      	butElem.appendChild(newDiv)
+      	butElem.appendChild(newDiv);
+
+      	var dispId = "digitDisplayId_" + digitDisplayId;
+      	var addSelect_1 = addSelectFunk(dispId);	// use the newly created digitDisplayId for the closure 
+    	
+    	var getAddButtId = document.getElementById("butt_" + buttIdAdd);	// get Id of newly created Add button
+    	getAddButtId.addEventListener("click", addSelect_1);	// add event listener to add button
     	};
+
  };
  
 
