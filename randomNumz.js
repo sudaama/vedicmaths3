f7690efda5f7f9761f54cc05da344e648b2f44bd
sudaama <sudaama@hotmail.com>
2017-10-04 18:35:25 +0100

    same same

---
+++ randomNumz.js
@@ -0,0 +1,14 @@
+
+
+var freshFrmId = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for form id
+var freshNumId = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for Num id
+var buttIdAdd = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for buttAdd id
+var buttIdRemove = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for buttRemove id
+var mixedId = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for mixedId id
+var ordinaryId = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for ordinaryId id
+var vinculumId = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for vinculumId id
+var digitDisplayId = Math.floor(Math.random() * 10000000) + 1;	// generate a random number for digitDisplay id
+var numberDisplayId = Math.floor(Math.random() * 10000000) + 1;  // generate a random number for numberDisplay id
+var numLabelId = Math.floor(Math.random() * 10000000) + 1;  // generate a random number for numLabelId id
+
+
