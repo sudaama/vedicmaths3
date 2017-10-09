57bad2aa8d9714b268f226566936b81d72861cb7
sudaama <sudaama@hotmail.com>
2017-10-09 18:16:04 +0100

    changed signDigitPropMod

--- signDigitPropMod.js
+++ signDigitPropMod.js
@@ -1,23 +1,73 @@
-// Function to create an array of signed digits 
-// Constructor for the SingleDigit grouping
+// Proper
 
 function SingleDigit(sign, digit) {
-	this.sign = sign;
-  	this.digit = digit;
-  	this.num = [this.sign, this.digit];
+    this.sign = sign;
+    this.digit = digit;
+    this.num = [this.sign, this.digit];
 }
 
+SingleDigit.prototype.displayDigits = function() {
+  return '{' + this.sign + ', ' + this.digit + '}';
+};
+
+
+// Constructor: build an array of SingleDigit objects 
+function ArrayDigits(mixedArr) {
+  var digitArr = mixedArr;
+  var arr3 = [];      // declare empty array here...better than global
+    for(var i = 0; i < digitArr.length/2; i++) { // fill in sign and digit
+      var temp = new SingleDigit(digitArr[i * 2 - 1], digitArr[i * 2]);
+      arr3.push(temp);
+    }
+   this.digitArray = arr3;
+}
+
+
+ArrayDigits.prototype.getNum = function() {
+  this.numz = []; // for each SingleDigit in the array of SingleDigit objects, get the digit part
+  this.signz = []; // for each SingleDigit in the array of SingleDigit objects, get the sign part
+  this.numProp = []; // Create an array of signed digits i.e. positive or negative 
+  
+  //console.log("digitArray length is: " + this.digitArray.length );  // test code 
+  for(var i = 0; i < this.digitArray.length; i++){
+    this.numz[i] = this.digitArray[i].digit;
+    this.signz[i] = this.digitArray[i].sign;
+    if(this.signz[i] !== '-') {
+      this.numProp[i] = this.numz[i];     // positive number
+    }
+    else {
+      this.numProp[i] = -this.numz[i];  // negative number
+    }
+  }
+  return this.numProp;            // return array of signed digits
+};
+
+
 var crtSingArr = function(formArray) {
-	return function() {
-		var arr3 = [];			// declare empty array here...better than global
-		for(var i = 0; i < formArray.length/2; i++) { // fill in sign and digit
-			var temp = new SingleDigit(formArray[i * 2], formArray[i * 2 + 1]);
-			arr3.push(temp);
-		}
-		return arr3;		// return arr3 to the function
-  	};
+  return function() {
+    var arr3 = [];      // declare empty array here...better than global
+    for(var i = 0; i < formArray.length/2; i++) { // fill in sign and digit
+      var temp = new SingleDigit(formArray[i * 2 - 1], formArray[i * 2]);
+      arr3.push(temp);
+    }
+    return arr3;    // return arr3 to the function
+    };
 };
 
 var createSingArray = crtSingArr(document.forms[0]);
 
 
+/* test data
+var digArr = ['+', 9, '-', 7, '+', 3, '-', 5];
+var digArr2 = ['+', 6, '-', 8, '+', 4, '-', 7];
+var digArr3 = ['-', 8, '-', 1, '+', 9, '-', 5];
+
+var newTing = new ArrayDigits(digArr);
+var newTing2 = new ArrayDigits(digArr2);
+var newTing3 = new ArrayDigits(digArr3);
+
+console.log(newTing.getNum());
+console.log(newTing2.getNum());
+console.log(newTing3.getNum());
+
+*/
